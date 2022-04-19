ccf773f98e94c5c142a0cdd4ca67daddff0a7da7
--local <sad100500@gmail.com>
2022-04-20 00:15:55 +0300

    copy update

--- src/Controllers/StartController.js
+++ src/Controllers/StartController.js
@@ -5,7 +5,7 @@ export default class StartController extends Controller {
     this.bot.onText(/\/start/, async (message) => {
       const chatId = message.chat.id;
 
-      await this.bot.sendMessage(chatId, 'Создай акканут в сервисе removebg - https://accounts.kaleido.ai/users/sign_in#api-key, затем получи API ключ, скопируй его, напиши мне /register и следуй инструкциям');
+      await this.bot.sendMessage(chatId, 'Создай акканут в сервисе removebg - https://accounts.kaleido.ai/users/sign_in#api-key, затем получи API ключ - https://www.remove.bg/dashboard#api-key, скопируй его, напиши мне /register и следуй инструкциям');
     });
   }
 }
